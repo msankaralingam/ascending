4e839f1a46089f143fdd519a983aa599b245f17c
Your Name <smile2sasi95@gmail.com>
2023-03-09 20:31:14 +0530

    add

---
+++ main.js
@@ -0,0 +1,42 @@
+var a=60;
+var b=50;
+var c=20;
+if((a<b)&&(a<c))
+{
+	if(b<c)
+	{
+		console.log(a,b,c);
+	}
+}
+else
+{
+	console.log(a,c,b);
+}
+
+if((b<a)&&(b<c))
+{
+	if(c<a)
+	{
+		
+	    console.log(b,c,a);
+	}
+}
+else
+{
+	    console.log(b,a,c);
+}
+
+if((c<a)&&(c<b));
+{
+	if(a<b)
+	{
+		console.log(c,b,a);
+	}
+	else
+	{
+		console.log(c,b,a);
+	}
+}
+
+	
+
